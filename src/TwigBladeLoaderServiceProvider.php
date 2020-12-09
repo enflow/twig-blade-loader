d4be0fccf9a00c64bcc43e6e0a7a3dbd51e3c8ea
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-12-09 11:46:49 +0100

    Move extension registration to service provider register call

--- src/TwigBladeLoaderServiceProvider.php
+++ src/TwigBladeLoaderServiceProvider.php
@@ -6,10 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class TwigBladeLoaderServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+         $this->app->afterResolving('twig', fn () => $this->app['twig']->addExtension(new TwigBladeLoaderExtension()));
+    }
+
     public function boot()
     {
         $this->app->singleton(BladeRenderer::class);
-
-        $this->app->afterResolving('twig', fn () => $this->app['twig']->addExtension(new TwigBladeLoaderExtension()));
     }
 }
